516bc60e09e172c6fc1e7fbc5f3a19763cdab45b
aviato26 <sdeshler426@aol.com>
2018-07-22 20:23:45 -0700

    added cheerio and csv modules

--- scraper.js
+++ scraper.js
@@ -1,1 +1,11 @@
-console.error('yo')
+let fs = require('fs');
+let cheerio = require('cheerio');
+let csv = require('csv');
+
+fs.readdir('data', function(e){
+  if(e !== null){
+    fs.mkdir('data', function(err){
+      console.error(err)
+    })
+  }
+})

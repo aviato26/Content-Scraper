47fa1f1ded6b4e052c4f308b063f974d448f7980
aviato26 <sdeshler426@aol.com>
2018-07-25 00:02:00 -0700

    added second request to get individual shirt pages

--- scraper.js
+++ scraper.js
@@ -1,3 +1,5 @@
+let http = require('http');
+let request = require('request');
 let fs = require('fs');
 let cheerio = require('cheerio');
 let csv = require('csv');
@@ -8,4 +10,17 @@ fs.readdir('data', function(e){
       console.error(err)
     })
   }
+})
+
+let shirts = {};
+let page;
+
+request('http://shirts4mike.com/shirts.php', function(err, res, html){
+  let $ = cheerio.load(html);
+  $('.products li').each((i, e) => {
+    page = e.children[0].attribs.href;
+    request(`http://shirts4mike.com/${page}`, function(err, res, html){
+      console.log(html)
+    })
+  })
 })

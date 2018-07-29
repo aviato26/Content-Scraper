97ca27a424ac7efe469c09b7fd21423132d64e59
aviato26 <sdeshler426@aol.com>
2018-07-28 21:06:25 -0700

    added if statement to get the completed properties for data array

--- scraper.js
+++ scraper.js
@@ -2,6 +2,7 @@ let request = require('request');
 let fs = require('fs');
 let cheerio = require('cheerio');
 let csv = require('json2csv').Parser;
+let shirtData;
 
 fs.readdir('data', function(e){
   if(e !== null){
@@ -15,6 +16,7 @@ let tableHead = ['Title', 'Price', 'ImageUrl', 'URL'];
 let paths = [];
 let counter;
 let a = new csv({tableHead});
+let b;
 let data = [];
 
 request('http://shirts4mike.com/shirts.php', function(err, res, html){
@@ -26,19 +28,25 @@ request('http://shirts4mike.com/shirts.php', function(err, res, html){
     counter = 0;
 
     request(`http://shirts4mike.com/${path}`, function(err, res, html){
-      let shirtData = cheerio.load(html);
-      data.push(
+        shirtData = cheerio.load(html);
+
+        data.push(
         {
           "Title": shirtData('title').text(),
-          "Price": shirtData('.price').text()
-          //shirtData('img').attr('src'),
-          //paths[counter++]
-        })
-        
-      fs.appendFile('./data/info.csv',data, (err) => {
+          "Price": shirtData('.price').text(),
+          "ImageURL": shirtData('img').attr('src'),
+          "URL": paths[counter++],
+        }
+      )
+
+      if(counter === 8){
+        b = a.parse(data);
+        fs.appendFile('./data/info.csv',b, (err) => {
         if(err)
         console.log('ddduuuuuhhhhhhh')
-      })
+        })
+      }
     })
   })
 })
+console.log(new Date().toISOString())
